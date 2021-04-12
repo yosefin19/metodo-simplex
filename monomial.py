7c795fcb7471393e258db7148d64bfca0559cb94
estudiante <yosi.solano19@gmail.com>
2021-04-12 01:10:34 -0600

    Correcion de interfaz

---
+++ monomial.py
@@ -0,0 +1,21 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class monomial():
+    def __init__(self,centralwidget):
+        self.signo = QtWidgets.QComboBox(centralwidget)
+        self.signo.addItem("+")
+        self.signo.addItem("-")
+        self.coefficient = QtWidgets.QLineEdit(centralwidget)
+        self.literal = QtWidgets.QLabel(centralwidget)
+
+class constrains():
+    def __init__(self,centralwidget):
+        self.monomials = []
+        self.operator = QtWidgets.QComboBox(centralwidget)
+        self.operator.addItem("<")
+        self.operator.addItem(">")
+        self.operator.addItem("=")
+        self.value = monomial(centralwidget)
+    
+    def get_monomials(self):
+        return self.monomials
